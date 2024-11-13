39cbf81eb240dde7de3320d66d6cf91730efd51e
Diego <diegosiqueira003@gmail.com>
2024-11-13 20:11:28 -0300

    Base url added

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -11,7 +11,7 @@ describe('Orange HRM Tests', () => {
   }
 
   it('Login - Sucess', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')//Visit indica qual o site estou verificando (e qual vamos entrar)
+    cy.visit('/auth/login')//Visit indica qual o site estou verificando (e qual vamos entrar)
     cy.get(selectorsList.usernameField).type (userData.userSucess.username) //O get indica qual o elemento do site que vc irá interagir e o Type, o que irá escrever nesse elemento que está interagindo(campo)
     cy.get(selectorsList.passawordField).type (userData.userSucess.password)// Idem ao anterior
     cy.get(selectorsList.submitField).click()//selecionou o botão de entrar com o get e o click indica clicar no botão selecionado pelo get
@@ -19,7 +19,7 @@ describe('Orange HRM Tests', () => {
     cy.get (selectorsList.dashboardGrind)//A última forma que utilizamos para checar se estamos no local correto é verificar na página algum elemento que apareça exclusivamente nessa página, nesse caso o método.
   })
   it('Login - Fail', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type ('xxx')
     cy.get(selectorsList.passawordField).type ('xxxx')
     cy.get(selectorsList.submitField).click()
